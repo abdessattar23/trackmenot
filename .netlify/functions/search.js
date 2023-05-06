572518cf450dd9de31fdbcdd2fc2e55c8423c441
Abdessattar Elyagoubi <106479409+abdessattar23@users.noreply.github.com>
2023-05-06 16:07:09 +0200

    Create search.js

---
+++ .netlify/functions/search.js
@@ -0,0 +1,28 @@
+const axios = require('axios');
+exports.handler = async (event, context) => {
+try {
+const base = 'https://cors-anywhere.herokuapp.com/';
+const url = event.queryStringParameters.url;
+  if(url){
+  const all = base + url;
+  const response = await axios.get(all);
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*', 
+        'Content-Type': 'text/html',
+      },
+      body: response,
+    };
+  }
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 500,
+      headers: {
+        'Content-Type': 'text/plain',
+      },
+      body: error.message,
+    };
+  }
+};
