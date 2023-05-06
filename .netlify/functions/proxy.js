c4ad271af01661665ea9826eb54dee8447d20b19
Abdessattar Elyagoubi <106479409+abdessattar23@users.noreply.github.com>
2023-05-06 21:29:56 +0200

    Update proxy.js

--- .netlify/functions/proxy.js
+++ .netlify/functions/proxy.js
@@ -1,16 +1,31 @@
 const axios = require('axios');
+const cheerio = require('cheerio');
 
 exports.handler = async function(event, context) {
   const url = event.queryStringParameters.url;
   if (!url) return { statusCode: 401, body: 'Please provide a URL' };
   try {
     const response = await axios.get(url);
+    const $ = cheerio.load(response.data);
+    const results = [];
+
+    $('div.links_main').each((i, element) => {
+      const $element = $(element);
+      const title = $element.find('h2.result__title a').text();
+      const iconLink = 'https:' + $element.find('span.result__icon a').attr('href');
+      const snippet = $element.find('a.result__snippet').text();
+      const linkElement = $element.find('h2.result__title a');
+      const link = linkElement.attr('href').match(/https:\/\/[^&]+/)[0];
+
+      results.push({ title, iconLink, snippet, link });
+    });
+
     return {
       statusCode: response.status,
-      body: response.data,
+      body: JSON.stringify(results),
       headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Content-Type': 'text/html',
+        'Access-Control-Allow-Origin': '.netlify.app',
+        'Content-Type': 'application/json',
       },
     };
   } catch (error) {
@@ -18,7 +33,7 @@ exports.handler = async function(event, context) {
       statusCode: error.response.status,
       body: JSON.stringify(error.response.data),
       headers: {
-        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Origin': '.netlify.app',
         'Content-Type': 'application/json',
       },
     };
