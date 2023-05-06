772539d9158239d1bcfd227a6e4b6369d5e67c0c
Abdessattar Elyagoubi <106479409+abdessattar23@users.noreply.github.com>
2023-05-06 23:00:57 +0200

    Update proxy.js

--- .netlify/functions/proxy.js
+++ .netlify/functions/proxy.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
+const detectBrowser = require('detect-browser');
 
 exports.handler = async function(event, context) {
   const q = event.queryStringParameters.q;
@@ -10,13 +11,21 @@ exports.handler = async function(event, context) {
     const $ = cheerio.load(response.data);
     const results = [];
 
+    const userAgent = event.headers['user-agent'];
+    const browser = detectBrowser.detect(userAgent);
+
+    if (browser) {
+      // Request is coming from a browser
+      return { statusCode: 403, body: 'Direct access not allowed' };
+    }
+
     $('div.links_main').each((i, element) => {
       const $element = $(element);
       const title = $element.find('h2.result__title a').text();
       const iconLink = 'https:' + $element.find('span.result__icon img').attr('src');
       const snippet = $element.find('a.result__snippet').text();
       const linkElement = $element.find('h2.result__title a');
-      const link =  decodeURIComponent(linkElement.attr('href').match(/https[^&]+/)[0]);
+      const link = decodeURIComponent(linkElement.attr('href').match(/https[^&]+/)[0]);
 
       results.push({ title, iconLink, snippet, link });
     });
@@ -33,7 +42,7 @@ exports.handler = async function(event, context) {
     console.error(error);
     return {
       statusCode: error.response?.status || 500,
-      body: JSON.stringify(error.response?.data || { error: error.message }),
+      body: JSON.stringify(error.response?.data || { error: 'Internal Server Error' }),
       headers: {
         'Access-Control-Allow-Origin': '*',
         'Content-Type': 'application/json',
