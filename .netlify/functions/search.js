2dea0bfc092719c68e433482f4d59fe59af7949c
Abdessattar Elyagoubi <106479409+abdessattar23@users.noreply.github.com>
2023-05-06 17:31:45 +0200

    Update search.js

--- .netlify/functions/search.js
+++ .netlify/functions/search.js
@@ -3,10 +3,23 @@ const cors_proxy = require('cors-anywhere');
 const host = '0.0.0.0';
 const port = process.env.PORT || 8080;
 
-cors_proxy.createServer({
-  originWhitelist: [], // Allow all origins
-  requireHeader: ['origin', 'x-requested-with'],
-  removeHeaders: ['cookie', 'cookie2']
-}).listen(port, host, () => {
-  console.log(`CORS Anywhere server running on ${host}:${port}`);
-});
+const handler = async (event, context) => {
+  return new Promise((resolve, reject) => {
+    cors_proxy.createServer({
+      originWhitelist: [], // Allow all origins
+      requireHeader: ['origin', 'x-requested-with'],
+      removeHeaders: ['cookie', 'cookie2']
+    }).addListener('request', (req, res) => {
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end('Hello World!\n');
+    }).listen(port, host, () => {
+      console.log(`CORS Anywhere server running on ${host}:${port}`);
+      resolve({
+        statusCode: 200,
+        body: 'CORS Anywhere server running on ${host}:${port}'
+      });
+    });
+  });
+};
+
+module.exports = { handler };
