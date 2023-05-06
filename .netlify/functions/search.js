35d52863a7819b89cb0565b52ca97ea3a0d4841d
Abdessattar Elyagoubi <106479409+abdessattar23@users.noreply.github.com>
2023-05-06 17:14:13 +0200

    Update search.js

--- .netlify/functions/search.js
+++ .netlify/functions/search.js
@@ -1,11 +1,10 @@
-var host = process.env.HOST || '0.0.0.0';
-// Listen on a specific port via the PORT environment variable
-var port = process.env.PORT || 8080;
-const cors = require('cors-anywhere');
-cors.createServer({
-    originWhitelist: [], // Allow all origins
-    requireHeader: ['origin', 'x-requested-with'],
-    removeHeaders: ['cookie', 'cookie2']
-}).listen(port, host, function() {
-    console.log('Running CORS Anywhere on ' + host + ':' + port);
+const corsAnywhere = require('cors-anywhere');
+const server = corsAnywhere.createServer({
+  originWhitelist: [], // Allow all origins
+  requireHeader: ['origin', 'x-requested-with'],
+  removeHeaders: ['cookie', 'cookie2']
+});
+
+exports.handler = server.listen(process.env.PORT || 3000, () => {
+  console.log(`Running CORS Anywhere on ${exports.handler.url}`);
 });
