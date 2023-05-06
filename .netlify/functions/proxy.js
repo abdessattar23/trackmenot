1eee48db6ebbd43d4e0782444c0d9190a0d1ec61
Abdessattar Elyagoubi <106479409+abdessattar23@users.noreply.github.com>
2023-05-06 18:37:28 +0200

    Update proxy.js

--- .netlify/functions/proxy.js
+++ .netlify/functions/proxy.js
@@ -2,15 +2,15 @@ const axios = require('axios');
 
 exports.handler = async function(event, context) {
   const { url } = event.queryStringParameters;
-  if (!url) return { statusCode: 400, body: 'Please provide a URL' };
+  if (!url) return { statusCode: 401, body: 'Please provide a URL' };
   try {
     const response = await axios.get(url);
     return {
       statusCode: response.status,
-      body: JSON.stringify(response.data),
+      body: response.data,
       headers: {
         'Access-Control-Allow-Origin': '*',
-        'Content-Type': 'application/json',
+        'Content-Type': 'text/plain',
       },
     };
   } catch (error) {
